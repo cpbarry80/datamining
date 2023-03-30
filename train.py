ec31526c6876c7a1775a113deea023ae3ba9981d
Biff Barry <cpbarry80@gmail.com>
2023-03-30 18:07:36 -0400

    sdv

--- train.py
+++ train.py
@@ -58,57 +58,69 @@ def get_meal_data(first=True):
 
 
 
-
-meal, fast = get_meal_data()
-secondmeal, secondfast = get_meal_data(first=False)
-
-
-
-def get_feature_matrix(meal_data):
-    '''FEATURES 
+def get_feature_matrix(meal, nans=7, s1=9, e1=24, s2=20, e2=23, fulllength=30):
+    '''
+    args: nans: number of nans allowed in a row
+    s1: start of meal
+    e1: end of meal
+    s2: start of fast
+    e2: end of fast
+    
+    FEATURES 
     https://www.coursera.org/learn/cse572/lecture/MPLNQ/project-2-machine-model-training-introductory-video-2
     1. time difference between when the meal was taken versus when the CGM reached maximum
     2. ( CGM max (aka Dg) - CGM meal ) / CGM meal   
     3. fast fourier transform....in this case, what we'll have is you will have the power at.... So you have four different numbers.
-        a. frequency F1
+        a. poweruency F1
         b. F1 
-        c. power at frequency,F2 
+        c. power at poweruency,F2 
         d. F2
-    4. slope of cgm right at meal time is very important feature. just subtract 2 diff data points
+    4. slope of cgm right at meal time is very important feature. just subtract 2 consecutive data points
     5. lets say after that you get a difference, you take another difference. the double difference is proprotional to the meal amount'''
 
 
-    meal_data = meal_data[meal_data.isnull().sum(axis=1) < 7].interpolate(method='linear',axis=1)
-    meal_data = meal_data[meal_data.isnull().sum(axis=1) < 1].reset_index(drop=True)
+    meal = meal[meal.isnull().sum(axis=1) < nans].interpolate(method='linear',axis=1, limit_direction="both")
+    meal = meal[meal.isnull().sum(axis=1) < 1].reset_index(drop=True)
 
-    freq_f1=[]
+    power_f1=[]
     f1=[]
-    freq_f2=[]
+    power_f2=[]
     f2=[]
 
-    tm=meal.iloc[:,22:25].idxmin(axis=1)
-    maximum=meal.iloc[:,5:19].idxmax(axis=1)
+
+    maximum = meal.iloc[:,s1:e1].idxmax(axis=1)
+    tm = meal.iloc[:,s2:e2].idxmin(axis=1)
+
     diff=[]
     diff2=[]
 
 
     for i in range(len(meal)):
-        array=abs(rfft(meal.iloc[:,0:30].iloc[i].values.tolist())).tolist()
-        sorted_array=abs(rfft(meal.iloc[:,0:30].iloc[i].values.tolist())).tolist()
-        sorted_array.sort()
-        freq_f1.append(sorted_array[-2])
-        freq_f2.append(sorted_array[-3])
-        f1.append(array.index(sorted_array[-2]))
-        f2.append(array.index(sorted_array[-3]))
+        fft = abs(rfft(meal.iloc[i].values))
+        sort_fft = fft.copy()
+        sort_fft.sort(reverse=True)
+
+        f1power = sort_fft[1] #skip the first one
+        f1location = fft.index(f1power)
+        f2power = sort_fft[2]
+        f2location = fft.index(f2power)
+
+        power_f1.append(f1power)
+        power_f2.append(f2power)
+        f1.append(f1location)
+        f2.append(f2location)
 
         diff.append(np.diff(meal.iloc[:,int(maximum[i]):int(tm[i])].iloc[i].tolist()).max())
         diff2.append(np.diff(np.diff(meal.iloc[:,int(maximum[i]):int(tm[i])].iloc[i].tolist())).max())
 
     matrix=pd.DataFrame()
-    matrix['tau_time'] = (meal.iloc[:,22:25].idxmin(axis=1)-meal.iloc[:,5:19].idxmax(axis=1))*5
-    matrix['difference_in_glucose_normalized'] = (meal.iloc[:,5:19].max(axis=1)-meal.iloc[:,22:25].min(axis=1))/(meal.iloc[:,22:25].min(axis=1))
-    matrix['freq_f1'] = freq_f1
-    matrix['freq_f2'] = freq_f2
+    matrix['tau'] = (meal.iloc[:,s1:e2].idxmin(axis=1) - meal.iloc[:,s1:e1].idxmax(axis=1)) * 5
+    #cgm max - cgm min
+    matrix['glucose_diff_normalized'] = (meal.iloc[:,s1:e1].max(axis=1) - meal.iloc[:,s2:e2].min(axis=1)) / (meal.iloc[:,s2:e2].min(axis=1))
+    
+    
+    matrix['power_f1'] = power_f1
+    matrix['power_f2'] = power_f2
     matrix['f1'] = f1
     matrix['f2'] = f2
     matrix['1stDifferential'] = diff
@@ -117,7 +129,22 @@ def get_feature_matrix(meal_data):
     return matrix
 
 
-get_feature_matrix(meal)
+
+
+
+meal, fast = get_meal_data()
+secondmeal, secondfast = get_meal_data(first=False)
+
+matrix_firstmeal = get_feature_matrix(meal)
+matrix_first_no_meal = get_feature_matrix(fast, s1=0, e1=19, s2=0, e2=24, fulllength=24)
+
+matrix_secondmeal = get_feature_matrix(secondmeal, fulllength=24)
+matrix_secondfast = get_feature_matrix(secondfast, s1=0, e1=19, s2=0, e2=24, fulllength=24)
+
+
+
+
+
 # validate the machine aka model
 # 1. train the model on 80% of the data
 # 2. test the model on 20% of the data
